4a7c6c05340d3ec549bb9b2b2c8879fe49aef790
adrianDelRio <45372134+adrianDelRio@users.noreply.github.com>
2022-05-12 19:54:39 +0200

    Frontend adjustment

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,18 +1,22 @@
-function handleSubmit(event) {
-    event.preventDefault();
-    const data = new FormData(event.target);
-    const value = Object.fromEntries(data.entries());
-    value.topics = data.getAll("topics");
+const form = document.getElementById("form");
+form.addEventListener("submit", submitForm);
 
-    // Mostramos los datos (simulando varios)
-    console.log({ value });
-
-    var show = document.getElementById("muestraDatos");
-    for (i = 0; i < 5; i++) {
-        show.innerHTML += "<p id='datosSimulados'>El titulo " + i + " es: " + value.title + "<br/>"
-        + "La URL " + i + " es: " + value.url + "<br/><p>";
-    }
+function submitForm(e) {
+    e.preventDefault();
+    const title = document.getElementById("title");
+    const url = document.getElementById("url");
+    const image = document.getElementById("image");
+    const formData = new FormData();
+    formData.append("title", title.value);
+    formData.append("url", url.value);
+    formData.append("image", image.value);
+    fetch("http://127.0.0.1:3000/upload_bookmark", {
+        method: 'POST',
+        body: formData,
+        headers: {
+          "Content-Type": "multipart/form-data"
+        }
+    })
+        .then((res) => console.log(res))
+        .catch((err) => ("Error occured", err));
 }
-
-const form = document.querySelector("form");
-form.addEventListener("submit", handleSubmit);
